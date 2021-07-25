95b3f6985f30417a26f9c1590dfb7bd37afbf608
Maro <81166378+miirmoon@users.noreply.github.com>
2021-07-25 18:13:00 +0900

    [SWEA] 2027. 대각선 출력하기
    화면에 보이는 그대로 출력하기

---
+++ SWExpertAcademy/SWEA_D1/Solution_2027.java
@@ -0,0 +1,18 @@
+package com.miirmoon.swea.d1;
+
+/**
+ * SWEA 2027. 대각선 출력하기
+ * Date: 2021. 7. 25.
+ * @author MIRAE
+ */
+
+public class Solution_2027 {
+
+	public static void main(String[] args) {
+		System.out.printf("#++++\n" + 
+				"+#+++\n" + 
+				"++#++\n" + 
+				"+++#+\n" + 
+				"++++#");
+	}
+}
